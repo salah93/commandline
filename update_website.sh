e90598d49e65d7eb0de8fe4cd2034ed80212752a
salah ahmed <salah93@crosscompute.com>
2017-03-25 10:01:32 -0400

    add script to update blog

---
+++ update_website.sh
@@ -0,0 +1,16 @@
+name=$1
+shift
+tags=
+target=~/Projects/salah93.github.io/_posts/commandline/`ds`-$name.md
+cat <<EOF > $target 
+---
+layout: post
+category : command-line
+tagline:
+tags : [commandline, unix, $(echo $@ | sed 's/ /, /')]
+---
+{% include JB/setup %}
+
+EOF
+
+cat $name >> $target 
